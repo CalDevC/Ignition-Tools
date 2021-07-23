b42bf06e57f6fc3e01561adb8f9e29cbfaada3f5
Chase Alexander <ChaseAlexander@ussupi.com>
2021-07-23 12:29:22 -0700

    Added alarm updater

---
+++ alarm_tag_updater.py
@@ -0,0 +1,30 @@
+"""
+Fill in the constant variables at the top of this file with the 
+information for your project and run the script in your project's 
+script console.
+"""
+
+ATTRIBUTE    = "" # the name of the alarm attribute you wish to update
+NEW_VALUE    = "" # the new value for your specified attribute
+PATH         = "[default]" # the path of the direcory you want to make the changes in 
+                          # (NOTE: ALL SUB-DIRECTORIES WILL BE AFFECTED UNLESS YOU SET THE ALTER_SUBDIR TO FALSE)
+                          # if you want to make changes to every tag in your project then provide the name of your project's tag provider (include square brackets)
+ALTER_SUBDIR = True # set to false if you do not want the changes to affect all subdirectories within your specified path
+
+def updateAlarmTags(directory, attribute, new_value, alter_subdirectories):
+	path_set = set()
+	for path in system.tag.browse(directory):
+		path_set.add(path["fullPath"])
+		
+	for path in path_set:
+		tag_info = system.tag.getConfiguration(path)[0]
+		
+		if u'alarms' in tag_info:
+			for alarm in tag_info[u'alarms']:
+				alarmConfig = {alarm[u'name']: [ [ attribute, "Value", new_value] ]}
+				system.tag.editAlarmConfig([path] , alarmConfig )
+		
+		if str(tag_info[u'tagType']) == 'Folder' and alter_subdirectories:
+			updateAlarmTags(str(tag_info[u'path']), attribute, new_value)
+			
+updateAlarmTags(PATH, ATTRIBUTE, NEW_VALUE, ALTER_SUBDIR)
